916bd956e81cb32294425c3620eab2cd1c14ad17
Nafis <nafissharifullin78@gmail.com>
2022-12-09 04:27:07 +0300

    Исправил ошибку

--- src/main/java/FifthTask.java
+++ src/main/java/FifthTask.java
@@ -1,21 +1,21 @@
-//Напишите программу на Java для подсчета количества
-// конкретных слов в строке, используя HashMap.
+import java.util.HashMap;
 
 
-public class HashMap {
+public class FifthTask{
     public static void main(String[] args) {
-        String st = "Every time, every time you tell me you gon' change";
+
+        String st = "Current task posted for Java developers developers";
         String[] words = st.split(" ");
 
         HashMap<String,Integer> keyValue = new HashMap<String,Integer>();
 
-        for (int i = 0; i <= words.length-1; i++) {
+        for (int i=0; i<= words.length-1; i++) {
             if (keyValue.containsKey(words[i])) {
                 int counter = keyValue.get(words[i]);
                 keyValue.put(words[i], counter+1);
             }
             else {
-                keyValue.put(words[i],1);
+                keyValue.put(words[i], 1);
             }
         }
         System.out.println(keyValue);
