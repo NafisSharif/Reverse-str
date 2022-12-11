663b100be11d578f63837f772dba4cd3f1e23ee2
Nafis <nafissharifullin78@gmail.com>
2022-12-12 00:18:01 +0300

    true

--- src/main/java/explicitWait.java
+++ src/main/java/explicitWait.java
@@ -1,23 +1,18 @@
-package Codes;
-
-
-
-
 import java.util.concurrent.TimeUnit;
-
-
 
 import org.openqa.selenium.By;
 
-import org.openqa.selenium.Keys;
-
 import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.WebElement;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 //Напишите программу на Java, чтобы продемонстрировать явную проверку условий ожидания.
